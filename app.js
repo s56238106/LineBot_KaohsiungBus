eaa6e3eb71b88172fee8d5636a38c6529e62edc0
chi <benson26365156@gmail.com>
2017-04-24 22:04:49 +0800

    app.js update

---
+++ app.js
@@ -0,0 +1,19 @@
+const linebot = require('linebot');
+
+const bot = linebot({
+	channelId: '1483029082',
+	channelSecret: 'ada6218baf7780aea777e87de0b5c093',
+	channelAccessToken: 'HgmqnvzXuSTtXjB/3yiI0TRFOD2JWImFRFhHavnhGvBLfTeDzBdEREYYWb+oT8zsTfxIBvP4JHHT8kQP853zcJ870pGvKDyKc+zi4cd3ebkitUY8xUa9dkFpIjDjvdNXO5AfquvILjUZ2FLxSluXrwdB04t89/1O/w1cDnyilFU='
+});
+
+bot.on('message', function (event) {
+    switch (event.message.type) {
+		case 'text':
+		    console.log(event.message.text);
+			break;
+    }
+});
+
+bot.listen('/linewebhook', process.env.PORT || 80, function () {
+	console.log('LineBot is running.');
+});
