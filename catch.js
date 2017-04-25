dfe62029c76ec8e4ba914178389010ce6e4e87df
母白 水 <s56238106@gm.kuas.edu.tw>
2017-04-25 21:24:53 +0800

    update all

---
+++ catch.js
@@ -0,0 +1,25 @@
+/*jshint esversion: 6 */
+var request = require('request');
+
+const busgetdata = (plate,callback) => {
+   request('http://ptx.transportdata.tw/MOTC/v2/Bus/EstimatedTimeOfArrival/City/Kaohsiung?$filter=PlateNumb%20eq%20%27'+plate+'%27&$format=JSON', 
+		(error, response, body) => {
+	  		if (!error && response.statusCode == 200) {
+	 	    		var bb =JSON.parse(body);
+	 	    		var aa = [];
+	 	    		for (var i = 0; i < bb.length; i++) {
+	 	    			aa[i]=[];
+	 	    			aa[i][0]=bb[i].EstimateTime;
+	 	    			aa[i][1]=bb[i].StopUID;
+	 	    			
+	 	    		}
+	 	    		callback(aa);
+	  		}
+	});
+};
+
+
+
+
+
+exports.busgetdata=busgetdata;
--- test.js
+++ test.js
@@ -1,4 +1,7 @@
 /*jshint esversion: 6 */
+
+
+//LINEBOT CONNECTION
 const linebot = require('linebot');
 
 const bot = linebot({
@@ -7,10 +10,33 @@ const bot = linebot({
 	channelAccessToken: 'HgmqnvzXuSTtXjB/3yiI0TRFOD2JWImFRFhHavnhGvBLfTeDzBdEREYYWb+oT8zsTfxIBvP4JHHT8kQP853zcJ870pGvKDyKc+zi4cd3ebkitUY8xUa9dkFpIjDjvdNXO5AfquvILjUZ2FLxSluXrwdB04t89/1O/w1cDnyilFU='
 });
 
+//CATCH DATA
+var data = require('./catch.js');
+
+var bus = data.busgetdata('127-FT',(data) => {
+	for (var i = 0; i < data.length; i++) {
+		for (var j = 0; j < data[i].length; j++) {
+			if (data[i][j]=='KHH5224') {
+				console.log(data[i]);
+			}
+		}
+		
+	}
+});
+
+
+
+//LINEBOT ACTION
 bot.on('message', function (event) {
     switch (event.message.type) {
 		case 'text':
-		    event.reply('Unknow message: ' + JSON.stringify(event));
+		    event.reply('Unknow message: ' + JSON.stringify(event).text);
+
+
+
+
+
+
 			break;
     }
 });
@@ -18,3 +44,4 @@ bot.on('message', function (event) {
 bot.listen('/linewebhook', process.env.PORT || 80, function () {
 	console.log('LineBot is running.');
 });
+
