7211bb415cc16471e1b5822640995d2016e0cb8f
母白 水 <s56238106@gm.kuas.edu.tw>
2017-04-26 21:09:07 +0800

    put data into action

--- template.js
+++ template.js
@@ -1,66 +1,83 @@
 /*jshint esversion: 6 */
 
 var stopdata = require('./catch.js');
+var one_template = JSON.stringify(require('./template.json'));
 
-var busID='127-FT';
-stopdata.busgetdata(busID,(data)=>{
 
-var a =
-{"type": "template",
-  "altText": "this is a carousel template",
-  "template": {
-      "type": "carousel",
-      "columns": [
-          {
-            "thumbnailImageUrl": "https://example.com/bot/images/item1.jpg",
-            "title": "this is menu",
-            "text": "description",
-            "actions": [
-                {
-                    "type": "postback",
-                    "label": "Buy",
-                    "data": "action=buy&itemid=111"
-                },
-                {
-                    "type": "postback",
-                    "label": "Add to cart",
-                    "data": "action=add&itemid=111"
-                },
-                {
-                    "type": "uri",
-                    "label": "View detail",
-                    "uri": "http://example.com/page/111"
-                }
-            ]
-          },
-          {
-            "thumbnailImageUrl": "https://example.com/bot/images/item2.jpg",
-            "title": "this is menu",
-            "text": "description",
-            "actions": [
-                {
-                    "type": "postback",
-                    "label": "Buy",
-                    "data": "action=buy&itemid=222"
-                },
-                {
-                    "type": "postback",
-                    "label": "Add to cart",
-                    "data": "action=add&itemid=222"
-                },
-                {
-                    "type": "uri",
-                    "label": "View detail",
-                    "uri": "http://example.com/page/222"
-                }
-            ]
-          }
-      ]
-  }
+//丟DATA進ACTION
+const template = (data) =>{
+	var one_template_temp = JSON.parse(one_template);
+	for(var i=0 ; i<data.length ; i++){
+		one_template_temp.actions.push(
+			{type : 'postback' ,label : data[i][0] + data[i][2] ,data : data[i][1]}
+		);
+	}
+	console.log(one_template_temp);
 };
 
-console.log(a.type);
 
-});
+//資料比數邏輯判斷
+const template_all = (data, num) =>{
+	var three_data = [];
+	for (var i = num; i < data.length; i++) {
+		if (i%12==0 && i!=num) {
+			three_data[0]=[];
+			three_data[0][0]="後";
+			three_data[0][1]=i;
+			three_data[0][2]=data.length-i+"筆";
+			three_data[1]=[];
+			three_data[1][0]="";
+			three_data[1][1]="";
+			three_data[1][2]="";
+			three_data[2]=[];
+			three_data[2][0]="";
+			three_data[2][1]="";
+			three_data[2][2]="";
+			template(three_data);
+			//template_all(data,i);
+			break;
+		}
+		else{
+			three_data[i%3]=[];
+			three_data[i%3][0]=data[i][0];
+			three_data[i%3][1]=data[i][1];
+			three_data[i%3][2]=data[i][2];
+			if (i%3==2) {
+				template(three_data);
+			}
+			else if (data.length-1==i) {
+				var count=i%3;
+				for (var j = 0; j < count; j++) {
+					three_data[2-j]=[];
+					three_data[2-j][0]="";
+					three_data[2-j][1]="";
+					three_data[2-j][2]="";
+				}
+				template(three_data);
+			}
+			
+		}
+	}
+};
 
+//線上測試
+/*var bus =stopdata.busgetdata('137-FT',(data) => {
+	template_all(data,48);
+	for (var i = 0; i < data.length; i++) {
+		console.log(data[i]);
+	}
+	
+});*/
 
+//JSON測試
+var bus = require('./testbus.json');
+
+var aa = [];
+for (var i = 0; i < bus.length; i++) {
+	aa[i]=[];
+	aa[i][0]=bus[i].StopName.Zh_tw;
+	aa[i][1]=bus[i].StopUID;
+	aa[i][2]=bus[i].EstimateTime;
+	
+}
+template_all(aa, 12);
