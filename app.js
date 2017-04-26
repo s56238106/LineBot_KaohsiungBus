f1fc6e699d6a9d6ecd0c8a1fadcbff1c33851331
母白 水 <s56238106@gm.kuas.edu.tw>
2017-04-26 23:27:21 +0800

    template & postback test

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const bot = linebot({
 	channelAccessToken: 'HgmqnvzXuSTtXjB/3yiI0TRFOD2JWImFRFhHavnhGvBLfTeDzBdEREYYWb+oT8zsTfxIBvP4JHHT8kQP853zcJ870pGvKDyKc+zi4cd3ebkitUY8xUa9dkFpIjDjvdNXO5AfquvILjUZ2FLxSluXrwdB04t89/1O/w1cDnyilFU='
 });
 
-var busID = '129-FT';
+var busID = '133-FT';
 var stopUID = "";
 
 //bot action
@@ -18,25 +18,15 @@ bot.on('message', function (event) {
     switch (event.message.type) {
 		case 'text':
                 template.Template(busID, (BusData) => {
-                    console.log(BusData);
                     event.reply(BusData);
                 });
-		  /*  var bus =data.busgetdata(busID,(data) => {
-				var num = -1;
-				for (var i = 0; i < data.length; i++) {
-					if (data[i][1] == event.message.text) {
-						num = i;
-					}
-				}
-				if(num != -1){
-		    		event.reply(data[num][0] + "\r\n" + data[num][1] + "\r\n" + data[num][2]);
-				}else{
-		    		event.reply("not found:404");
-				}
-		    });
-		    event.reply(a);*/
 			break;
     }
+});
+
+bot.on('postback', function(event){
+    console.log(event.postback.data);
+    stopUID = event.postback.data;
 });
 
 bot.listen('/linewebhook', process.env.PORT || 80, function () {
